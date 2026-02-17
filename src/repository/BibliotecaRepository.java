9bb8b3fe51a44f1431dd6f2a5937205ee7941921
Lucas Moura <felipelucasmoura350@gmail.com>
2026-02-17 19:40:48 -0300

    feat: implementação do repository em memória utilizando streams API

--- src/repository/BibliotecaRepository.java
+++ src/repository/BibliotecaRepository.java
@@ -1,4 +1,58 @@
 package repository;
 
+import model.Emprestimo;
+import model.ItemBiblioteca;
+import model.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class BibliotecaRepository {
+
+    private List<Pessoa> pessoas;
+    private List<ItemBiblioteca> itens;
+    private List<Emprestimo> emprestimos;
+
+    public BibliotecaRepository() {
+        this.pessoas = new ArrayList<>();
+        this.itens = new ArrayList<>();
+        this.emprestimos = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+
+    public Optional<Pessoa> buscarPessoaPorCpf(String cpf) {
+        return pessoas.stream()
+                .filter(p -> p.getCpf().equals(cpf))
+                .findFirst();
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void adicionarItem(ItemBiblioteca item) {
+        itens.add(item);
+    }
+
+    public Optional<ItemBiblioteca> buscarItemPorCodigo(int codigo) {
+        return itens.stream()
+                .filter(i -> i.getCodigo() == codigo)
+                .findFirst();
+    }
+
+    public List<ItemBiblioteca> getItens() {
+        return itens;
+    }
+
+    public void adicionarEmprestimo(Emprestimo emprestimo) {
+        emprestimos.add(emprestimo);
+    }
+
+    public List<Emprestimo> getEmprestimo() {
+        return emprestimos;
+    }
 }
